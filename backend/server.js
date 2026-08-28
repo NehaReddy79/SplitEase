56040fd49c92d59482ca13e35bf5ee10001de861
Neha Reddy <nehareddy200709@gmail.com>
2026-08-28 12:05:01 +0530

    Adding User model and setting up server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log('server file ready')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("connected");
+    })
+    .catch((err) => {
+        console.log("MongoDb connection error" , err);
+    });
+
+app.get('/' , (req , res) => {
+    res.send("Api is running");
+});
+app.listen(process.env.PORT , () =>{
+    console.log(`server running on port ${process.env.PORT}`)
+})
