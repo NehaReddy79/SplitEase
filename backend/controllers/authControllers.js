5e7d7117ff544b8504566cf0f32f36038e6e2789
Neha Reddy <nehareddy200709@gmail.com>
2026-08-28 14:07:47 +0530

    Adding login route with jwt auth

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 async function signUp(req, res) {
 
@@ -17,10 +18,39 @@ async function signUp(req, res) {
             res.status(201).json({ message: "User created successfully" })
 
         }
-    }catch(error){
+    } catch (error) {
         console.error(error.message)
         res.status(500).json({ error: "Something went wrong" });
     }
+
+}
+
+async function login(req, res) {
+
+    try {
+        const { email, password } = req.body
+        const userRes = await User.findOne({ email })
+        if (userRes) {
+            if (await bcrypt.compare(password, userRes.password)) {
+
+                const token = jwt.sign(
+                    { id: userRes._id },
+                    process.env.JWT_SECRET,
+                    { expiresIn: '7d' }
+                )
+
+                res.status(200).json({ token, userId: userRes._id })
+            } else {
+                res.status(400).json({ error: "Invalid credentials" })
+            }
+        } else {
+            res.status(400).json({ error: "Invalid credentials" })
+        }
+    }catch(error){
+        console.error(error.message)
+        res.status(500).json({error : "Something went wrong"})
+    }
     
 }
-module.exports = signUp
+
+module.exports = { signUp, login }
