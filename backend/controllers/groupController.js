b1ca2e16dce6e21f0b9c12d3f629bb0aea073565
Neha Reddy <nehareddy200709@gmail.com>
2026-08-31 14:46:56 +0530

    Adding addMember route to groups

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -2,17 +2,43 @@ const Group = require('../models/Group')
 
 async function createGroup(req, res) {
     try {
-        const {name} = req.body
+        const { name } = req.body
 
         const groupRes = new Group({ name, createdBy: req.userId, members: [req.userId] })
 
         await groupRes.save();
         res.status(201).json({ message: "Group created successfully" })
     }
-    catch(error){
+    catch (error) {
         console.error(error.message)
-        res.status(500).json({error : "Something went wrong"})
+        res.status(500).json({ error: "Something went wrong" })
     }
 
 }
-module.exports = createGroup;
+
+async function addMember(req, res) {
+    try {
+        const { groupId } = req.params
+        const { userId } = req.body
+
+        const groupRes = await Group.findById(groupId)
+
+        if (!groupRes) {
+            return res.status(404).json({ error: "Group doesn't exist" })
+        }
+        if (groupRes.members.includes(userId)) {
+            return res.status(400).json({ error: "User already exists in the group" })
+        }
+        groupRes.members.push(userId)
+        await groupRes.save()
+
+        res.status(200).json(groupRes)
+    }catch(error){
+        console.error(error.message)
+        res.status(500).json({error : "Something went wrong"})
+    }
+    
+
+}
+
+module.exports = { createGroup, addMember };
--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const verifyToken = require('../middleware/authMiddleware')
-const createGroup = require('../controllers/groupController')
+const {createGroup , addMember} = require('../controllers/groupController')
 
 router.post('/' , verifyToken , createGroup)
+router.post('/:groupId/members' , verifyToken , addMember)
 
 module.exports = router;
