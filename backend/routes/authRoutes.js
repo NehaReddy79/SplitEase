ae1626bf26aa6a00778a28ce102202040ad5f02d
Neha Reddy <nehareddy200709@gmail.com>
2026-09-02 11:29:12 +0530

    Adding rate limiting to auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const { signUp , login } = require('../controllers/authControllers')
 const verifyToken = require('../middleware/authMiddleware')
+const authLimiter = require('../middleware/rateLimiter')
 
-router.post('/signup' , signUp)
-router.post('/login' , login)
+router.post('/signup' ,authLimiter, signUp)
+router.post('/login' ,authLimiter, login)
 
 router.get('/test-protected', verifyToken, (req, res) => {
     res.json({ message: "You are authenticated", userId: req.userId });
