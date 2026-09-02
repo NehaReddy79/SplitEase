8ef91898e7c7826e4d632016b568be6f7dabaeb3
Neha Reddy <nehareddy200709@gmail.com>
2026-09-02 13:43:29 +0530

    Set up Socket.io server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const http = require('http')
+const {Server} = require('socket.io')
 const authRoutes = require('./routes/authRoutes')
 const groupRoutes = require('./routes/groupRoutes')
 const expenseRoutes = require('./routes/expenseRoutes')
@@ -28,7 +30,28 @@ app.use('/api/groups' , groupRoutes)
 app.use('/api/expenses' , expenseRoutes)
 app.use('/api/settlements' , settlementRoutes)
 
+const server = http.createServer(app)
+const io = new Server(server , {
+    cors : {
+        origin : "*"
+    }
+})
 
-app.listen(process.env.PORT , () =>{
-    console.log(`server running on port ${process.env.PORT}`)
+app.set('io',io)
+
+io.on('connection' , (socket) =>{
+    console.log('A user connected : ' , socket.id)
+
+    socket.on('joinGroup' , (groupId) =>{
+        socket.join(groupId)
+        console.log(`Socket ${socket.id} joined group ${groupId}`)
+    })
+
+    socket.on('disconnect', () =>{
+        console.log('User disconnected : ', socket.id)
+    })
+})
+
+server.listen(process.env.PORT , () =>{
+    console.log(`Server running on port ${process.env.PORT}`)
 })
