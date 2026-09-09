0298a8085b2f8cab1d74771d07f779e87b24b2e5
Neha Reddy <nehareddy200709@gmail.com>
2026-09-09 09:33:21 +0530

    Adding group layout

--- frontend/src/pages/GroupLayout.jsx
+++ frontend/src/pages/GroupLayout.jsx
@@ -1,19 +1,25 @@
-import {Outlet , Link , useParams} from 'react-router-dom'
+import { Outlet, NavLink, useParams } from 'react-router-dom'
+import './GroupLayout.css'
 
-export function GroupLayout(){
-    const {groupId} = useParams()
+export function GroupLayout() {
+    const { groupId } = useParams()
 
-    return(
-        <div>
-            <h2>Group</h2>
-            <nav>
-                <Link to={`/groups/${groupId}`}>Overview</Link> || {' '}
-                <Link to={`/groups/${groupId}/expenses`}>Expenses</Link> || {' '}
-                <Link to={`/groups/${groupId}/balances`}>Balances</Link> || {' '}
-                <Link to={`/groups/${groupId}/settlements`}>Settlements</Link> || {' '}
+    return (
+        <div className='group-page'>
+            <nav className='group-nav'>
+                <h1><span>SplitEase</span></h1>
             </nav>
-            <hr />
-            <Outlet />
+            <div className='group-content'>
+                <div className='group-tabs'>
+                        <NavLink to={`/groups/${groupId}`} end className={({isActive}) => isActive ? 'active' : ''}>
+                            Overview
+                        </NavLink>
+                        <NavLink to={`/groups/${groupId}/expenses`} end className={({isActive}) => isActive ? 'active' : ''}>Expenses</NavLink>
+                        <NavLink to={`/groups/${groupId}/balances`} end className={({isActive}) => isActive ? 'active' : ''}>Balances</NavLink> 
+                        <NavLink to={`/groups/${groupId}/settlements`} end className={({isActive}) => isActive ? 'active' : ''}>Settlements</NavLink> 
+                </div>
+                <Outlet />
+            </div>
         </div>
     )
 }
